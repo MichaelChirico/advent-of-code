dfad0d82ada44d66c91f40d49aac7d8fd655f8cf
Michael Chirico <michaelchirico4@gmail.com>
2024-12-05 02:11:04 +0000

    revisit 03 Pt. 1 in anticipation of moving on to Pt. 2

--- 2024/03.R
+++ 2024/03.R
@@ -1,15 +1,15 @@
-input = readLines('input-data/03')
+input = readChar('input-data/03', file.size('input-data/03'))
+
+## PART ONE
 
 # my first instinct
-mul_idx = gregexpr(R"(mul\((?<l>[0-9]{1,3}),(?<r>[0-9]{1,3})\))", input, perl=TRUE)
+mul_idx = gregexpr(R"(mul\((?<l>[0-9]{1,3}),(?<r>[0-9]{1,3})\))", input, perl=TRUE)[[1L]]
 
-sum(mapply(input, mul_idx, USE.NAMES=FALSE, FUN=function(str, idx) {
-  s = attr(idx, "capture.start")
-  n = attr(idx, "capture.length")
-  l = as.integer(substring(str, s[,"l"], s[,"l"]+n[,"l"]-1L))
-  r = as.integer(substring(str, s[,"r"], s[,"r"]+n[,"r"]-1L))
-  sum(l*r)
-}))
+s = attr(mul_idx, "capture.start")
+n = attr(mul_idx, "capture.length")
+l = as.integer(substring(input, s[,"l"], s[,"l"]+n[,"l"]-1L))
+r = as.integer(substring(input, s[,"r"], s[,"r"]+n[,"r"]-1L))
+sum(l*r)
 
 # aided by rex
 library(rex)
@@ -18,28 +18,19 @@ match_df = input |>
     "mul(",
     capture(between(digit, 1, 3), name="l"), ",",
     capture(between(digit, 1, 3), name="r"), ")"
-  ))
-sum(mapply(input, match_df, USE.NAMES=FALSE, FUN=function(s, DF) with(DF, {
-  lv = as.integer(substring(s, l.start, l.end))
-  rv = as.integer(substring(s, r.start, r.end))
+  )) |>
+  unlist(recursive = FALSE)
+with(match_df, {
+  lv = as.integer(substring(input, l.start, l.end))
+  rv = as.integer(substring(input, r.start, r.end))
   sum(lv * rv)
-})))
+})
 
 # parse() approach
-mul_idx = gregexpr(R"(mul\([0-9]{1,3},[0-9]{1,3}\))", input, perl=TRUE)
+mul_idx = gregexpr(R"(mul\([0-9]{1,3},[0-9]{1,3}\))", input, perl=TRUE)[[1L]]
 mul = `*`
-eval(parse(text = paste(collapse = "+", unlist(mapply(input, mul_idx, USE.NAMES=FALSE, SIMPLIFY=FALSE,
-  FUN=\(s, idx) substring(s, idx, idx+attr(idx, "match.length")-1L)
-)))))
-
-# strsplit() approach
-ul=mul
 input |>
-  strsplit(R"[m(?=ul\([0-9]{1,3},[0-9]{1,3}\))]", perl=TRUE) |>
-  unlist() |>
-  grep("^ul", x=_, value=TRUE) |> # 1st element might not match
-  sub("\\).*", ")", x=_) |>
+  substring(mul_idx, mul_idx+attr(mul_idx, "match.length")-1L) |>
   paste(collapse = "+") |>
-  parse(text = _, keep.source=FALSE) |>
+  parse(text = _, keep.source = FALSE) |>
   eval()
-
