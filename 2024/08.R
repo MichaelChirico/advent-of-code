c0bf1d792826caf8f98c3029e8ff1b92eda07aae
Michael Chirico <michaelchirico4@gmail.com>
2024-12-08 08:13:44 +0000

    Pt. 2 done

--- 2024/08.R
+++ 2024/08.R
@@ -7,11 +7,12 @@ freqs = input[input != "."] |>
   Filter(f = \(n) n>1L) |>
   names()
 
-is_antinode = matrix(FALSE, nrow(input), ncol(input))
+between = \(x, bounds) x >= bounds[1L] & x <= bounds[2L]
 e <- environment()
 upper_bounds = dim(input)
 
-between = \(x, bounds) x >= bounds[1L] & x <= bounds[2L]
+## PART ONE
+is_antinode = matrix(FALSE, nrow(input), ncol(input))
 for (freq in freqs) {
   node_locs = which(input == freq, arr.ind=TRUE)
   combn(nrow(node_locs), 2L, simplify=FALSE, function(ij) {
@@ -27,3 +28,32 @@ for (freq in freqs) {
     NULL
   })
 }
+sum(is_antinode)
+
+## PART TWO
+is_antinode = matrix(FALSE, nrow(input), ncol(input))
+for (freq in freqs) {
+  node_locs = which(input == freq, arr.ind=TRUE)
+  combn(nrow(node_locs), 2L, simplify=FALSE, function(ij) {
+    node_pair = node_locs[ij, ]
+    delta = apply(node_pair, 2L, diff)
+    all_antinodes = rbind(
+      cbind(
+        seq(to = node_pair[1L, "row"], by=delta[1L], length.out=upper_bounds[1L]),
+        seq(to = node_pair[1L, "col"], by=delta[2L], length.out=upper_bounds[2L])
+      ),
+      cbind(
+        seq(from = node_pair[2L, "row"], by=delta[1L], length.out=upper_bounds[1L]),
+        seq(from = node_pair[2L, "col"], by=delta[2L], length.out=upper_bounds[2L])
+      )
+    )
+    all_antinodes = all_antinodes[
+      between(all_antinodes[, 1L], c(1L, upper_bounds[1L])) &
+      between(all_antinodes[, 2L], c(1L, upper_bounds[2L])),,
+      drop=FALSE
+    ]
+    e$is_antinode[all_antinodes] = TRUE
+    NULL
+  })
+}
+sum(is_antinode)
