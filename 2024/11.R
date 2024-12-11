86932af0722f39f34b23a516f3c5a97321652dfc
Michael Chirico <michaelchirico4@gmail.com>
2024-12-11 05:42:31 +0000

    Done Pt. 2

--- 2024/11.R
+++ 2024/11.R
@@ -10,7 +10,22 @@ apply_rule = function(x) {
   c(x %/% pow, x %% pow)
 }
 apply_rules = function(x) {
-  unlist(lapply(x, apply_rule))
+  lapply(x, apply_rule)
 }
 
-for (ii in 1:25) stones = apply_rules(stones)
+## PART ONE
+for (ii in 1:25) stones = unlist(apply_rules(stones))
+
+## PART TWO
+
+library(data.table)
+stones = data.table(value = input)[, .(freq=as.numeric(.N)), keyby=value]
+for (ii in 1:75) {
+  stones = stones[, {
+    next_value = apply_rules(value)
+    data.table(
+        value = unlist(next_value),
+        freq = rep(freq, lengths(next_value))
+    )[, .(freq=sum(freq)), keyby=value]
+  }]
+}
