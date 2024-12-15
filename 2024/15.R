3d2bb7d7950f586d50086417ed25c9c44fcbad83
Michael Chirico <michaelchirico4@gmail.com>
2024-12-15 07:01:24 +0000

    copy-paste version...

--- 2024/15.R
+++ 2024/15.R
@@ -33,13 +33,19 @@ map = head(input, gap-1L) |>
   strsplit(NULL) |>
   do.call(what = rbind)
 
+## PART ONE
+
+map = head(input, gap-1L) |>
+  strsplit(NULL) |>
+  do.call(what = rbind)
+
 walls = matrix(map == "#", nrow(map), ncol(map))
 boxes = matrix(map == "O", nrow(map), ncol(map))
 
 bot_idx = which(map == "@", arr.ind=TRUE)
 
 for (rule in rules) {
-  before = map
+  # before = map
   step = movements[[rule]]
   switch(map[bot_idx + step],
     `.` = {
@@ -73,3 +79,56 @@ for (rule in rules) {
 box_idx = which(map == "O", arr.ind=TRUE)
 
 sum(100L*(box_idx[,"row"]-1L) + box_idx[,"col"]-1L)
+
+## PART TWO
+
+map_widener = list(
+  `#` = c("#", "#"),
+  `O` = c("[", "]"),
+  `.` = c(".", "."),
+  `@` = c("@", ".")
+)
+wide_map = head(input, gap-1L) |>
+  strsplit(NULL) |>
+  lapply(\(row) unlist(map_widener[row], use.names=FALSE)) |>
+  do.call(what = rbind)
+
+walls = matrix(wide_map == "#", nrow(wide_map), ncol(wide_map))
+box_left = matrix(wide_map == "[", nrow(wide_map), ncol(wide_map))
+box_right = matrix(wide_map == "[", nrow(wide_map), ncol(wide_map))
+
+bot_idx = which(wide_map == "@", arr.ind=TRUE)
+
+for (rule in rules) {
+  # before = wide_map
+  step = movements[[rule]]
+  switch(wide_map[bot_idx + step],
+    `.` = {
+      wide_map[bot_idx] = "."
+      bot_idx = bot_idx + step
+      wide_map[bot_idx] = "@"
+    },
+    `#` = {
+      # do nothing
+    },
+    `[` = , `]` = {
+      k = 2L
+      repeat {
+        switch(wide_map[bot_idx + k*step],
+          `.` = {
+            browser()
+            shifted_idx = do.call(rbind, sapply(0:k, \(kk) bot_idx + kk*step, simplify=FALSE))
+            shifted = wide_map[shifted_idx]
+            wide_map[shifted_idx] = c(tail(shifted, 1L), head(shifted, -1L))
+            bot_idx = bot_idx + step
+            break
+          },
+          `[` = , `]` = { browser(); k = k + 1L },  # keep looking
+          `#` = break # do nothing: move impossible
+        )
+      }
+    }
+  )
+  # show_snapshot(before, wide_map, rule)
+}
+
