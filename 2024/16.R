8ad3b170fe9741a7d6a402dd11d1a386689e0219
Michael Chirico <michaelchirico4@gmail.com>
2024-12-17 05:35:12 +0000

    first time finishing the loop

--- 2024/16.R
+++ 2024/16.R
@@ -9,7 +9,10 @@ draw_path = function(path) {
     input[path[1:(nrow(path)-1L),, drop=FALSE]] = "."
     input[path[nrow(path),, drop=FALSE]] = "X"
   }
-  apply(input, 1L, paste, collapse="") |> writeLines()
+  draw_range = apply(path, 2L, range)
+  draw_range[] = draw_range + c(-1L, 1L)
+  draw = input[draw_range[1L]:draw_range[2L], draw_range[3L]:draw_range[4L]]
+  apply(draw, 1L, paste, collapse="") |> c("") |> writeLines()
 }
 
 start_idx = which(input == "S", arr.ind=TRUE)
@@ -26,19 +29,29 @@ step_touches = function(step, prev_steps) {
 }
 
 paths = list(start_idx)
-n_completed_paths=0L
+all_tread_idx = matrix(nrow = sum(input %in% c(".", "S")), ncol=2L)
+colnames(all_tread_idx) = c("row", "col")
+all_tread_idx[prev_tread_idx<-1L, ] = start_idx
+n_completed_paths = 0L
+max_steps = 1L
+run_env <- environment()
 repeat {
   n_new_paths = 0L
   for (ii in seq_along(paths)) {
     path = paths[[ii]]
     last_step = path[nrow(path),, drop=FALSE]
     if (all(last_step == end_idx)) {
+      paths[[ii]] = list(paths[[ii]])
       next
     }
     next_path = lapply(1:4, function(jj) {
       next_step = last_step + step_delta[jj,, drop=FALSE]
-      if (input[next_step] != "#" && !step_touches(next_step, path)) {
-        if (step_touches(next_step, end_idx)) n_completed_paths <<- n_completed_paths + 1L
+      if (input[next_step] != "#" && !step_touches(next_step, all_tread_idx[1:prev_tread_idx,, drop=FALSE])) {
+        if (step_touches(next_step, end_idx)) {
+          run_env$n_completed_paths = run_env$n_completed_paths + 1L
+        } else {
+          run_env$all_tread_idx[run_env$prev_tread_idx<-run_env$prev_tread_idx+1L, ] = next_step
+        }
         rbind(path, next_step)
       }
     })
@@ -46,9 +59,7 @@ repeat {
     n_new_paths = n_new_paths + sum(cont_idx)
     paths[[ii]] = next_path[cont_idx]
   }
-  if (!n_new_paths) break
   paths = unlist(paths, recursive=FALSE)
-  if (any(sapply(paths, anyDuplicated))) stop("Some paths with duplicates")
-  if (max(sapply(paths, nrow)) %% 50L == 0L) browser()
-  cat(sprintf("%d new/continued paths, %d total, max(path_length)=%d, %d completed\n", n_new_paths, length(paths), max(sapply(paths, nrow)), n_completed_paths))
+  if (!n_new_paths) break
+  max_steps = max_steps + 1L
 }
