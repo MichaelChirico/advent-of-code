3c3c34db63ee0a87907df2e8af0896eabd0968bd
Michael Chirico <michaelchirico4@gmail.com>
2024-12-19 08:25:33 +0000

    Looks like the way to go: buckets

--- 2024/19.R
+++ 2024/19.R
@@ -59,27 +59,31 @@ total_matches = 0L
 possible_blocks = patterns_full[
   sapply(patterns_full, \(ptn) any(grepl(ptn, designs_feasible, fixed=TRUE)))
 ]
-possible_patterns = data.table(pattern = possible_blocks)
+possible_patterns = data.table(pattern = possible_blocks, N = 1L)
 
 repeat {
   possible_patterns[, exact_match := rowSums(outer(pattern, designs_feasible, `==`))]
-  total_matches = total_matches + sum(possible_patterns$exact_match)
+  total_matches = total_matches + possible_patterns[, sum(exact_match * N)]
   # NB: no design is a substring of another design!
   possible_patterns = possible_patterns[exact_match == 0]
-  possible_patterns = possible_patterns[, .(pattern = c(
-    outer(pattern, possible_blocks, paste0)
-  ))]
+  possible_patterns = possible_patterns |>
+    _[, .(
+      pattern = c(outer(pattern, possible_blocks, paste0)),
+      N = rep(N, each=length(possible_blocks))
+    )] |>
+    _[, .(N = sum(N)), keyby=pattern]
   possible_patterns = possible_patterns[nchar(pattern) <= max_dsn]
   # only calculate this once per pattern (while retaining duplicate rows if the same
   #   pattern can come from different sources)
-  possible_patterns[, any_match := any(startsWith(designs_feasible, .BY$pattern)), by=pattern]
+  possible_patterns[, any_match := sapply(pattern, \(ptn) any(startsWith(designs_feasible, ptn)))]
   # idea: cache !any_match rows -- they are likely to come up again. e.g.
   #   if 'a|bcde' produced 'abcde' & found infeasible, we know 'ab|c|de'
   #   will also be infeasible --> no need to re-check. unsure how common.
   possible_patterns = possible_patterns[(any_match)]
+  if (!nrow(possible_patterns)) break
   cat(sprintf(
-    "%d patterns under construction, size range %s. %d matches found.\n",
-    nrow(possible_patterns),
+    "%10.0f patterns under construction (%5d unique), size range %s. %5.0f matches found.\n",
+    sum(possible_patterns$N), nrow(possible_patterns),
     toString(range(nchar(possible_patterns$pattern))),
     total_matches
   ))
