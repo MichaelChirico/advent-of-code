b51ecda505ff503e9d162528018b4e8c9328856d
Michael Chirico <michaelchirico4@gmail.com>
2024-12-19 08:14:16 +0000

    Looks promising: build patterns from the left only -- allows much more aggressive trimming at each step

--- 2024/19.R
+++ 2024/19.R
@@ -52,35 +52,35 @@ feasible = mapply(
 sum(feasible)
 
 ## PART TWO
-n_splits_at_any = function(chars) {
-  sum(sapply(
-    seq_len(min(length(chars), max_ptn)),
-    \(char_i) n_splits_at_char(chars, char_i)
+designs_feasible = designs_full[feasible]
+max_dsn = max(nchar(designs_feasible))
+total_matches = 0L
+
+possible_blocks = patterns_full[
+  sapply(patterns_full, \(ptn) any(grepl(ptn, designs_feasible, fixed=TRUE)))
+]
+possible_patterns = data.table(pattern = possible_blocks)
+
+repeat {
+  possible_patterns[, exact_match := rowSums(outer(pattern, designs_feasible, `==`))]
+  total_matches = total_matches + sum(possible_patterns$exact_match)
+  # NB: no design is a substring of another design!
+  possible_patterns = possible_patterns[exact_match == 0]
+  possible_patterns = possible_patterns[, .(pattern = c(
+    outer(pattern, possible_blocks, paste0)
+  ))]
+  possible_patterns = possible_patterns[nchar(pattern) <= max_dsn]
+  # only calculate this once per pattern (while retaining duplicate rows if the same
+  #   pattern can come from different sources)
+  possible_patterns[, any_match := any(startsWith(designs_feasible, .BY$pattern)), by=pattern]
+  # idea: cache !any_match rows -- they are likely to come up again. e.g.
+  #   if 'a|bcde' produced 'abcde' & found infeasible, we know 'ab|c|de'
+  #   will also be infeasible --> no need to re-check. unsure how common.
+  possible_patterns = possible_patterns[(any_match)]
+  cat(sprintf(
+    "%d patterns under construction, size range %s. %d matches found.\n",
+    nrow(possible_patterns),
+    toString(range(nchar(possible_patterns$pattern))),
+    total_matches
   ))
 }
-
-n_splits_at_char = function(chars, char_i) {
-  before = as.list(head(chars, char_i))
-  names(before) = paste0("V", seq_along(before))
-  before = c(before, size = char_i)
-  match_before = patterns[before, on=names(before), nomatch=NULL]
-  if (!nrow(match_before)) return(0L)
-  if (length(chars) == char_i) return(1L)
-  # NB: inefficiently keeps trying, e.g. if 'bg' fails,
-  #   we know 'bgg' will also fail. ignore for now...
-  return(sum(
-    match_before[, by=.I, .(amt = n_splits_at_any(tail(chars, -size)))]$amt
-  ))
-}
-
-# https://github.com/Rdatatable/data.table/issues/6668
-options(datatable.verbose=FALSE) 
-total = mapply(
-  function(ii, nc) {
-    if (ii %% 50L == 0L) cat(sprintf("design=%d\n", ii))
-    n_splits_at_any(design_chars[ii, seq_len(nc)])
-  },
-  seq_along(designs_full), nchar(designs_full)
-)
-
-sum(total)
