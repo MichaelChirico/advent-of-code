82d2d6e4d67426550e1136f76f6994c010cd4f1c
Michael Chirico <michaelchirico4@gmail.com>
2024-12-19 06:34:54 +0000

    more munging

--- 2024/19.R
+++ 2024/19.R
@@ -3,7 +3,11 @@ input = readLines('input-data/19-test')
 
 patterns_full = sort(unlist(strsplit(input[1L], ",\\s+")))
 patterns = setDT(tstrsplit(patterns_full, NULL))
-setkey(patterns)
+patterns[, full := patterns_full]
+patterns[, size := nchar(full)]
+max_ptn = max(nchar(patterns_full))
+setkeyv(patterns, paste0("V", seq_len(max_ptn)))
+setindexv(patterns, lapply(seq_len(max_ptn), \(j) c(paste0("V", 1:j), "size")))
 
 designs_full = tail(input, -2L)
 design_chars = designs_full |>
@@ -14,28 +18,28 @@ design_chars = designs_full |>
   as.matrix() |>
   unname()
 
-can_split_at = function(chars, char_i) {
-  if (char_i > ncol(patterns)) return(0L)
-  before = as.list(head(chars, char_i))
-  can_before = length(patterns[before, which=TRUE]) > 0L
-  if (!can_before) return(-1L)
-  if (length(chars) == char_i) return(1L)
-  n_after = 1L
-  repeat {
-    val = can_split_at(tail(chars, -char_i), n_after)
-    if (val == 1L) return(TRUE)
-    if (val == -1L) break
-    n_after = n_after + 1L
+can_split_at = function(chars, char_i=NULL) {
+  if (is.null(char_i)) {
+    char_i = seq_along(chars)
   }
-  return(0L)
+  if (length(char_i) > 1L) {
+    return(any(sapply(char_i, can_split_at, chars=chars)))
+  }
+  if (char_i > max_ptn) return(FALSE)
+  before = as.list(head(chars, char_i))
+  names(before) = paste0("V", seq_along(before))
+  before = c(before, size = char_i)
+  match_before = patterns[before, on=names(before)]
+  if (!nrow(match_before)) return(FALSE)
+  if (length(chars) == char_i) return(TRUE)
+  unmatched_chars = tail(chars, -char_i)
+  # NB: inefficiently keeps trying, e.g. if 'bg' fails,
+  #   we know 'bgg' will also fail. ignore for now...
+  match_before[, by=.I, can_split_at(tail(chars, -size), NULL)]
+  return(FALSE)
 }
 
-feasible = sapply(seq_along(designs_full), function(ii) {
-  any(sapply(
-    seq_len(nchar(designs_full)[ii]),
-    \(char_i) can_split_at(design_chars[ii, ], char_i)
-  ))
-})
-  char_i = 1L
-  pattern_subset = patterns[design_chars[ii, char_i], which=TRUE]
-}
+feasible = mapply(
+  \(ii, nc) can_split_at(design_chars[ii, ], seq_len(nc)),
+  seq_along(designs_full), nchar(designs_full)
+)
